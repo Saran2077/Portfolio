7499506ea61f48f15913635dcbdb17ebaff07b55
Saran M <saran.m@adya.ai>
2025-04-13 22:55:16 +0530

    image location changed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,7 +12,7 @@ const About = () => {
             <div className="relative">
               <div className="w-64 h-64 mx-auto rounded-full overflow-hidden border-4 border-blue-500/20">
                 <img
-                  src="src\assets\saran.jpg"
+                  src="src/assets/saran.jpg"
                   alt="Profile"
                   className="w-full h-full object-cover"
                 />
